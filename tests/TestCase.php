d80285b95de3a945ec0bcc5594b0e8a9070238b0
Fábián Dávid <david.fabian@herfhub.hu>
2026-03-24 09:58:10 +0100

    updated test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,14 @@
 namespace Akaunting\Money\Tests;
 
 use Akaunting\Money\Provider;
+use Illuminate\Testing\TestResponse;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    // Older Testbench releases expect the concrete test case to provide this.
+    protected static ?TestResponse $latestResponse = null;
+
     protected function getPackageProviders($app): array
     {
         return [Provider::class];
