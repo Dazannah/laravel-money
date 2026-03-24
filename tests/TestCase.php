86d58c60df9779c2c1709e7518e6ac38008fa496
Fábián Dávid <david.fabian@herfhub.hu>
2026-03-24 10:13:45 +0100

    testbench version 9 raised

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,14 +3,10 @@
 namespace Akaunting\Money\Tests;
 
 use Akaunting\Money\Provider;
-use Illuminate\Testing\TestResponse;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    // Older Testbench releases expect the concrete test case to provide this.
-    public static ?TestResponse $latestResponse = null;
-
     protected function getPackageProviders($app): array
     {
         return [Provider::class];
